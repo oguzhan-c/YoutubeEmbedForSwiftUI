c35168f09962b0a3c7282d76402fccbde9f48b14
Oğuzhan Can <oguzhan.can.iletisim@gmail.com>
2023-05-01 12:18:16 +0300

    Initial Commit

---
+++ YoutubeEmbedForSwiftUI/YoutubeEmbedForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeEmbedForSwiftUIApp.swift
+//  YoutubeEmbedForSwiftUI
+//
+//  Created by Oğuzhan Can on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeEmbedForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
